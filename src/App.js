089d63c6b6a905e063f7aaf5bccfb2706b12bca0
Brooke <brooke.xiang@gmail.com>
2020-08-27 03:03:38 -0700

    add todo for work 27 aug 2020

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Theme from "./theme/theme";
 import GlobalStyles from "./theme/globalStyles";
 import SideBar from "./components/SideBar";
+import CoverLetter from "./components/Main";
+import { lookup } from "dns";
 
 const AppWrapper = styled.div`
   border: 2px solid black;
@@ -11,12 +13,24 @@ const AppWrapper = styled.div`
   height: 100vh;
 `;
 
+// TODO:
+// 1. figure out what sections I need to write:
+// - about me;
+// - work experience/journey;
+// - why I want to work with Podyssey;
+// 2. implement react router so I can include:
+// - traditional resume;
+// - writing sample;
+// 3. do more work on UI/how I want it to lookup;
+// 4. add profile picture
+
 function App() {
   return (
     <Theme>
       <AppWrapper className="App">
         <GlobalStyles />
         <SideBar />
+        <CoverLetter />
       </AppWrapper>
     </Theme>
   );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 import SpotifyPlayer from "react-spotify-player";
+import { setAutoFreeze } from "immer";
 
 const StyledDiv = styled.div`
   width: 25%;
   height: 100vh;
   background-color: ${props => props.theme.color.mermista};
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h3,
+  p {
+    color: ${props => props.theme.color.wrongHordak};
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
 `;
-const StyledTitle = styled.h3`
-  font-size: 2rem;
-  color: ${props => props.theme.color.wrongHordak};
-`;
+
+const playerSize = {
+  height: "80px",
+  width: "100%"
+};
 
 const SideBar = () => {
   return (
     <StyledDiv>
-      <StyledTitle>Yifan (Brooke) Xiang</StyledTitle>
-      <p>hello, world</p>
+      <h3>Yifan (Brooke) Xiang</h3>
+      <p>Education</p>
+      <p>Favourite podcasts and why (3)</p>
+      <p>social media/where to find me</p>
       <SpotifyPlayer
         uri="spotify:episode:2SHRqwykgwVUN4QK2NCasY"
-        size="large"
+        size={playerSize}
         theme="black"
         view="list"
       />
