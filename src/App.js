b648f166a7de45c5964680a21426f11b9342cbef
Brooke <brooke.xiang@gmail.com>
2020-08-27 02:22:10 -0700

    delete font files to import google fonts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import SpotifyPlayer from "react-spotify-player";
-import GlobalFonts from "./fonts/fonts";
+import Theme from "./theme/theme";
+import GlobalStyles from "./theme/globalStyles";
+import SideBar from "./components/SideBar";
 
-const StyledContainer = styled.div`
+const AppWrapper = styled.div`
   border: 2px solid black;
   display: flex;
   flex-direction: row;
   height: 100vh;
-  background-color: #0f8cbd;
-  p {
-    font-family: "Montserrat";
-  }
 `;
 
-const StyledPlayer = styled.div`
-  border-radius: 5px;
-  border: 10px solid black;
-  height: fit-content;
-`;
 function App() {
   return (
-    <StyledContainer className="App">
-      <GlobalFonts />
-      <p>hello, world 2</p>
-      <StyledPlayer>
-        <SpotifyPlayer
-          uri="spotify:episode:2SHRqwykgwVUN4QK2NCasY"
-          size="large"
-          theme="black"
-          view="list"
-        />
-      </StyledPlayer>
-    </StyledContainer>
+    <Theme>
+      <AppWrapper className="App">
+        <GlobalStyles />
+        <SideBar />
+      </AppWrapper>
+    </Theme>
   );
 }
 
