f36c5e398b2c4950875d59b3568cfe38cb213f41
Brooke <brooke.xiang@gmail.com>
2020-08-26 22:04:32 -0700

    add fave ologies episode in player

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import SpotifyPlayer from "react-spotify-player";
+import GlobalFonts from "./fonts/fonts";
 
+const StyledContainer = styled.div`
+  border: 2px solid black;
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  background-color: #0f8cbd;
+  p {
+    font-family: "Montserrat";
+  }
+`;
+
+const StyledPlayer = styled.div`
+  border-radius: 5px;
+  border: 10px solid black;
+  height: fit-content;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledContainer className="App">
+      <GlobalFonts />
+      <p>hello, world 2</p>
+      <StyledPlayer>
+        <SpotifyPlayer
+          uri="spotify:episode:2SHRqwykgwVUN4QK2NCasY"
+          size="large"
+          theme="black"
+          view="list"
+        />
+      </StyledPlayer>
+    </StyledContainer>
   );
 }
 
