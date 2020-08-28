dccd16b6b5a805e74f79dc4397571eeee3c29612
Brooke <brooke.xiang@gmail.com>
2020-08-27 22:26:27 -0700

    update todo

--- src/App.js
+++ src/App.js
@@ -4,25 +4,21 @@ import Theme from "./theme/theme";
 import GlobalStyles from "./theme/globalStyles";
 import SideBar from "./components/SideBar";
 import CoverLetter from "./components/Main";
-import { lookup } from "dns";
 
 const AppWrapper = styled.div`
-  border: 2px solid black;
   display: flex;
   flex-direction: row;
   height: 100vh;
 `;
 
 // TODO:
-// 1. figure out what sections I need to write:
-// - about me;
-// - work experience/journey;
-// - why I want to work with Podyssey;
-// 2. implement react router so I can include:
+
+// 1. implement react router so I can include:
 // - traditional resume;
 // - writing sample;
-// 3. do more work on UI/how I want it to lookup;
-// 4. add profile picture
+// 2. do more work on UI/how I want it to lookup;
+// 3. add profile picture DONE
+// 4. complete sidebar components
 
 function App() {
   return (
