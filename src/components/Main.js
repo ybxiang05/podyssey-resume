0d0fc10fd51a2dac2364579b38f93dfe1ac6d50f
Brooke <brooke.xiang@gmail.com>
2020-08-27 22:26:47 -0700

    draft about me and half of why I want to work for podyssey

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
+//TODO:
+// 1. figure out what sections I need to write:
+// - about me; DONE
+// - work experience/journey;
+// - why I want to work with Podyssey;
+// - why Podyssey would want to work with me;
+
+const StyledDiv = styled.div`
+  margin-left: 25%;
+  padding: 5%;
+`;
 const CoverLetter = () => {
   return (
-    <div>
+    <StyledDiv>
+      <h3>About Me</h3>
+      <p>
+        Hi Melody and Danny, Thanks for taking the time to read my application. I thought about
+        submitting a traditional resume like you asked, but I wanted to create an opportunity where
+        I can show you the thoughts, values, and skills that make up who I am without the
+        constraints of a tired and clichéd form. More importantly, I wanted a chance to tell you my
+        story. After all, half the intrigue of a good podcast is in its storytelling.
+      </p>
+      <p>
+        Language has always been one of my deepest obsessions. I'm endlessly fascinated by the ways
+        in which words and the worlds we build with them can bring us together, drive us apart, and,
+        in its unique paradoxical logic, elicit the very emotions in us that we'd experience
+        differently if we didn't have the language to name them. This fascination resulted in my
+        pursuit of an undergraduate degree in English Literature at the University of Toronto
+        (2014), followed by a masters degree in English Language and Literatures at the University
+        of British Columbia (2018). More importantly, it brought me to my love of podcasts.
+      </p>
+      <h3>Why I want to work for Podyssey</h3>
+      <p>
+        I want to work for Podyssey for a lot of different reasons, but many of them have to do with
+        the pandemic. I voluntarily left a position as a junior developer with a Toronto-based
+        startup in June amidst the chaos of the pandemic and the growing outrage as result of the
+        systemic violences against BIPOC around the world. I wanted to give myself the time and
+        space to reflect on my relationship to capitalism and white supremacy. I also wanted to
+        reflect on how I can use my gift with words to help others connect in these strange times,
+        and in doing so, reconnect with myself and my values.
+      </p>
+      <h3>Why Podyssey should want to work with me</h3>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
         been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
@@ -13,7 +52,27 @@ const CoverLetter = () => {
         passages, and more recently with desktop publishing software like Aldus PageMaker including
         versions of Lorem Ipsum.
       </p>
-    </div>
+      <h3>Why Podyssey should want to work with me</h3>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+      <h3>Why Podyssey should want to work with me</h3>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+    </StyledDiv>
   );
 };
 
