48a5a533ec6c06bfa5f188dabd562dd851b8062e
Brooke <brooke.xiang@gmail.com>
2020-08-27 22:27:34 -0700

    standardize font sizes

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -6,9 +6,12 @@ body {
     padding: 0;
     margin: 0;
     font-family: 'Roboto', sans-serif;
+    font-size: 1.15rem;
 }
 h1, h2, h3, h4 {
     font-family: 'Montserrat', sans-serif;
+    font-size: 2rem;
+    font-weight: 600;
 }`;
 
 export default GlobalStyles;
