88a9fafbaac987e526009c686ab58828d1386320
Brooke <brooke.xiang@gmail.com>
2020-08-29 22:23:10 -0700

    Updates

---
+++ precache-manifest.67a795616ae3f75e599e5cf285037478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0116aca08307bd4f6215b83869c8be54",
+    "url": "/podyssey_resume/index.html"
+  },
+  {
+    "revision": "f021d2d4d02a708b8198",
+    "url": "/podyssey_resume/static/js/2.055fdbdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/podyssey_resume/static/js/2.055fdbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a52f33469b78ecd0605",
+    "url": "/podyssey_resume/static/js/main.f027a7b2.chunk.js"
+  },
+  {
+    "revision": "9ac6f4718f494447529e",
+    "url": "/podyssey_resume/static/js/runtime-main.b3bbf7d4.js"
+  },
+  {
+    "revision": "308c8d08694abe0e269afaecd069d8e3",
+    "url": "/podyssey_resume/static/media/brooke-img.308c8d08.jpeg"
+  }
+]);
