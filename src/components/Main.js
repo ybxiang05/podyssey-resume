d3d3295a276ccedc4f25d2a5fb8f4c34d208dded
Brooke <brooke.xiang@gmail.com>
2020-09-01 19:56:28 -0700

    change url to /home to fix blank screen problem on load?

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,7 +41,7 @@ const Main = () => {
   return (
     <StyledMain>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/digital-marketing-resume" component={DMResume} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/writing-sample" component={WritingSample} />
